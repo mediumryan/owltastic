db97974294f4a62598e6642927b16aa728c11378
Ryan <straightwind0726@gmail.com>
2023-10-19 16:31:30 +0900

    [Update] Generated Layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import { styled } from 'styled-components';
 import './CSS/index.css';
+import Header from './Components/Header/Header';
+import Intro from './Components/Intro/Intro';
+import Feature from './Components/Feature/Feature';
+import Greeting from './Components/Greeting/Greeting';
+import Recent from './Components/Recent/Recent';
+import Contact from './Contact/Contact';
+import Stay from './Stay/Stay';
+import Footer from './Footer/Footer';
+
+const OuterWrapper = styled.div`
+    background-color: var(--primary-200);
+`;
+
+const InnerWrapper = styled.div`
+    height: 100vh;
+    padding: var(--padding-medium-large);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
 
 function App() {
-    return <div className="App">hello,world</div>;
+    return (
+        <OuterWrapper>
+            <InnerWrapper>
+                <Header />
+                <Intro />
+                <Feature />
+                <Greeting />
+                <Recent />
+                <Contact />
+                <Stay />
+                <Footer />
+            </InnerWrapper>
+        </OuterWrapper>
+    );
 }
 
 export default App;
