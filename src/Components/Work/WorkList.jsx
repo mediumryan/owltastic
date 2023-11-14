a02e8742d9779813138ed93c56307a16442b4657
Ryan <straightwind0726@gmail.com>
2023-11-14 17:13:56 +0900

    [Update] Generated the WorkList component

--- src/Components/Work/WorkList.jsx
+++ src/Components/Work/WorkList.jsx
@@ -1,5 +1,118 @@
-import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { keyframes, styled } from 'styled-components';
+import { workListState } from '../../atom';
+
+const WorkListOuter = styled.section``;
+
+const scrollAnimation = keyframes`
+    from{
+        transform: translateY(0);
+    }
+    to{
+        transform: translateY(-85%);
+    }
+`;
+
+const WorkListInner = styled.div`
+    background-color: var(--brown-100);
+    padding: 8rem;
+    ul {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        li {
+            background: var(--brown-100);
+            box-sizing: border-box;
+            border: 0.5px solid var(--primary-100);
+            padding: 24px;
+            margin-bottom: 32px;
+            width: 46%;
+            a {
+                color: var(--primary-100);
+                text-decoration: none;
+                div:first-child {
+                    height: 364px;
+                    margin-bottom: 24px;
+                    overflow: hidden;
+                    img {
+                        width: 100%;
+                        border: 0.5px solid var(--primary-100);
+                        filter: grayscale(100%) invert(10%) sepia(8%)
+                            saturate(350%) hue-rotate(351deg) brightness(105%)
+                            contrast(95%);
+                        &:hover {
+                            filter: none;
+                            animation: ${scrollAnimation} 8s ease;
+                        }
+                    }
+                }
+                div:last-child {
+                    font-family: var(--font-family-normal);
+                    font-size: 0.8rem;
+                    letter-spacing: -0.2px;
+                    line-height: 2;
+                    display: flex;
+                    align-items: center;
+                    h4 {
+                        flex-basis: 70%;
+                        padding-right: 2rem;
+                    }
+                }
+            }
+            &:hover {
+                a {
+                    color: var(--primary-200);
+                }
+            }
+        }
+    }
+`;
+
+const SeeMore = styled.span`
+    font-family: var(--font-family-round);
+    display: flex;
+    justify-content: center;
+    padding: 0.25rem 0;
+    flex-basis: 30%;
+    border-radius: 16px;
+    border: 0.5px solid var(--primary-100);
+`;
 
 export default function WorkList() {
-    return <div>WorkList</div>;
+    const workList = useRecoilValue(workListState);
+
+    return (
+        <WorkListOuter>
+            <WorkListInner>
+                <ul>
+                    {workList.map((item) => {
+                        return (
+                            <li key={item.id}>
+                                <a href={item.url}>
+                                    <div>
+                                        <img
+                                            src={item.image_path}
+                                            alt="Project Poster"
+                                        />
+                                    </div>
+                                    <div>
+                                        <h4>
+                                            <span>
+                                                <strong>Client: </strong>
+                                                {item.client}
+                                                <br />
+                                                <strong>Project: </strong>
+                                                {item.content}
+                                            </span>
+                                        </h4>
+                                        <SeeMore>See more →</SeeMore>
+                                    </div>
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </WorkListInner>
+        </WorkListOuter>
+    );
 }
--- src/atom.js
+++ src/atom.js
@@ -7,7 +7,7 @@ import faculty from './Images/WorkImages/home-cropped.jpg';
 import garden from './Images/WorkImages/coaches-high-res.webp';
 import venture from './Images/WorkImages/vilcap-home-preview-desktop-high-res.webp';
 
-// header
+// for header
 
 export const headerMenuState = atom({
     key: 'header_menu_state',
@@ -45,7 +45,7 @@ export const headerMenuState = atom({
     ],
 });
 
-// home page
+// for home page
 
 export const workState = atom({
     key: 'work_state',
@@ -127,6 +127,128 @@ export const aboutState = atom({
             url: 'https://www.creativebloq.com/business/meagan-fisher-business-design-91412936',
             content:
                 'Featured Interview: Meagan Fisher on the Business of Design',
+        },
+    ],
+});
+
+// for work page
+
+export const workListState = atom({
+    key: 'work_list_state',
+    default: [
+        {
+            id: 0,
+            content: 'Audio Multimedia and Connected Services experiences',
+            image_path:
+                'https://owltastic.com/img/portfolio/toyota/connected-services-plans.webp',
+            url: 'https://owltastic.com/projects/toyota-product-redesign.html',
+            client: 'Toyota, via Saatchi & Saatchi',
+        },
+        {
+            id: 1,
+            content:
+                'Information architecture, content strategy, brand refresh, and marketing site redesign',
+            image_path:
+                'https://owltastic.com/img/portfolio/swell/swell-home.webp',
+            url: 'https://owltastic.com/projects/swell-marketing-redesign.html',
+            client: 'Swell, via Faculty',
+        },
+        {
+            id: 2,
+            content:
+                'UX design, product strategy, and visual design for a project management tool',
+            image_path:
+                'https://owltastic.com/img/portfolio/console/console-preview.webp',
+            url: 'https://owltastic.com/projects/console-product-design.html',
+            client: 'Faculty',
+        },
+        {
+            id: 3,
+            content: 'UX and visual design for a marketing website',
+            image_path:
+                'https://owltastic.com/img/portfolio/faculty/home-cropped.jpg',
+            url: 'https://owltastic.com/projects/faculty-agency-marketing-site.html',
+            client: 'Faculty',
+        },
+        {
+            id: 4,
+            content: 'Three portfolio design templates',
+            image_path:
+                'https://owltastic.com/img/portfolio/interior-design/homepage-preview.webp',
+            url: 'https://owltastic.com/projects/interior-designer-portfolio.html',
+            client: 'Adobe',
+        },
+        {
+            id: 5,
+            content: 'Marketing site design for a venture capital firm',
+            image_path:
+                'https://owltastic.com/img/portfolio/village-capital/home-preview.webp',
+            url: 'https://owltastic.com/projects/venture-capital-landing-page.html',
+            client: 'Village Capital',
+        },
+        {
+            id: 6,
+            content: 'Poetry discovery website and build',
+            image_path:
+                'https://owltastic.com/img/portfolio/verse/home-max.webp',
+            url: 'https://owltastic.com/projects/poetry-discovery-website.html',
+            client: 'Verse',
+        },
+        {
+            id: 7,
+            content: 'Web and product design for a home gardening startup',
+            image_path:
+                'https://owltastic.com/img/portfolio/gardenary/coaches-high-res.webp',
+            url: 'https://owltastic.com/projects/garden-startup-design.html',
+            client: 'Gardenary',
+        },
+        {
+            id: 8,
+            content: 'Design series focused on world-changing women',
+            image_path:
+                'https://owltastic.com/img/portfolio/celebrate-good/zitkala-sa-preview.webp',
+            url: 'https://owltastic.com/projects/celebrate-the-good-profiles.html',
+            client: 'Adobe',
+        },
+        {
+            id: 9,
+            content: 'Web and product design for a psychology product',
+            image_path: 'https://owltastic.com/img/portfolio/bcfg/preview.jpg',
+            url: 'https://owltastic.com/projects/behavior-change-product-app.html',
+            client: 'University of Pennsylvania',
+        },
+        {
+            id: 10,
+            content:
+                'Website designs and UI kits for nonprofit and ecommerce websites',
+            image_path:
+                'https://owltastic.com/img/portfolio/pet-booking/ui-kit.jpg',
+            url: 'https://owltastic.com/projects/adobe-ui-kits.html',
+            client: 'Adobe',
+        },
+        {
+            id: 11,
+            content: 'Website design for a fintech company',
+            image_path:
+                'https://owltastic.com/img/portfolio/fintech-website/website.jpg',
+            url: 'https://owltastic.com/projects/financial-software-website.html',
+            client: 'Openfin',
+        },
+        {
+            id: 12,
+            content: 'Landing page and advertising campaign',
+            image_path:
+                'https://owltastic.com/img/portfolio/scroll-landing-page/mobile.webp',
+            url: 'https://owltastic.com/projects/scroll-landing-page.html',
+            client: 'Scroll',
+        },
+        {
+            id: 13,
+            content: 'Bausaus inspired landing page design',
+            image_path:
+                'https://owltastic.com/img/portfolio/bauhaus-type/bauhaus-homepage.webp',
+            url: 'https://owltastic.com/projects/bauhaus-inspired-landing-page.html',
+            client: 'Adobe',
         },
     ],
 });
