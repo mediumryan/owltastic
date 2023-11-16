4ed565e56948d62f0c2d24ac2be9ebde260e407c
Ryan <straightwind0726@gmail.com>
2023-11-16 12:54:00 +0900

    [Update] The About page is done

--- src/Components/About/AboutInterview.jsx
+++ src/Components/About/AboutInterview.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import {
+    AboutContents,
+    AboutInner,
+    AboutItem,
+    AboutOuter,
+    AboutTitle,
+} from '../Home/About/About';
+import { HiExternalLink } from 'react-icons/hi';
+import { useRecoilValue } from 'recoil';
+import { aboutState } from '../../atom';
 
 export default function AboutInterview() {
-    return <div>AboutInterview</div>;
+    const aboutItem = useRecoilValue(aboutState);
+
+    return (
+        <AboutOuter>
+            <AboutInner>
+                <AboutTitle>Recent Interviews</AboutTitle>
+                <AboutContents>
+                    {aboutItem.slice(0, 3).map((item) => {
+                        return (
+                            <AboutItem>
+                                <a href={item.url}>
+                                    <p>
+                                        {item.title}
+                                        <HiExternalLink />
+                                    </p>
+                                    <h3>{item.content}</h3>
+                                </a>
+                            </AboutItem>
+                        );
+                    })}
+                </AboutContents>
+            </AboutInner>
+        </AboutOuter>
+    );
 }
--- src/Components/Home/About/About.jsx
+++ src/Components/Home/About/About.jsx
@@ -4,13 +4,13 @@ import { useRecoilValue } from 'recoil';
 import { aboutState } from '../../../atom';
 import { HiExternalLink } from 'react-icons/hi';
 
-const AboutOuter = styled.section`
+export const AboutOuter = styled.section`
     background: url(${aboutBg}) center no-repeat;
     background-size: cover;
     width: 100%;
 `;
 
-const AboutInner = styled.div`
+export const AboutInner = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -18,20 +18,20 @@ const AboutInner = styled.div`
     padding: 48px 24px;
 `;
 
-const AboutTitle = styled.h1`
+export const AboutTitle = styled.h1`
     font-size: 32px;
     color: var(--primary-100);
     margin-bottom: 24px;
 `;
 
-const AboutContents = styled.ul`
+export const AboutContents = styled.ul`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     grid-template-rows: repeat(1, 1fr);
     grid-column-gap: 1rem;
 `;
 
-const AboutItem = styled.li`
+export const AboutItem = styled.li`
     border: 1px solid var(--primary-100);
     padding: 2rem 3rem;
     background-color: var(--brown-100);
